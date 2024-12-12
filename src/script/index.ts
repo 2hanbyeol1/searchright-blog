c61f7459c9928bfb1d00956da9c36da95f7798ba
2hanbyeol1 <2hanbyeol1@naver.com>
2024-12-13 02:27:13 +0900

    refactor: readFileAsText 함수 사용, 함수 분리

--- src/script/index.ts
+++ src/script/index.ts
@@ -4,46 +4,57 @@ const $textarea = document.querySelector<HTMLTextAreaElement>("textarea")!;
 const $warning = document.querySelector<HTMLDivElement>("#warning")!;
 const $notionFileInput =
   document.querySelector<HTMLInputElement>("#notion-file-input")!;
+const $notionFile = document.getElementById("notion-file");
 
-document
-  .getElementById("notion-file")
-  ?.addEventListener("change", (e: Event) => {
-    const file = (e.target as HTMLInputElement)?.files?.[0];
-    if (!file) return;
+$notionFile?.addEventListener("change", (e: Event) => {
+  const file = (e.target as HTMLInputElement)?.files?.[0];
+  if (!file) return;
 
-    const reader = new FileReader();
+  (async () => {
+    let text = await readFileAsText(file).then((text) => processText(text));
 
-    reader.onload = (ev: ProgressEvent<FileReader>) => {
-      const result = ev.target?.result as ArrayBufferLike;
-      if (!result) return;
+    const $article = getArticleElementFromText(text);
+    if (!$article) return;
+    const processedHtml = processDOM($article);
 
-      const decoder = new TextDecoder();
-      const text = decoder.decode(new DataView(result));
-
-      const processedText = processText(text);
-
-      const domparser = new DOMParser();
-      const doc = domparser.parseFromString(processedText, "text/html");
-      const $article = doc.querySelector("article");
-      if (!$article) return "";
-
-      const processedHtml = processDOM($article);
-      $textarea.value = processedHtml;
-      $textarea.readOnly = false;
-
-      updateWarningMessage();
-      $notionFileInput.style.display = "none";
-    };
-
-    reader.readAsArrayBuffer(file);
-  });
-
-$textarea.addEventListener("keyup", () => {
-  updateWarningMessage();
+    $textarea.value = processedHtml;
+    $textarea.readOnly = false;
+    updateWarningMessage(processedHtml);
+    $notionFileInput.style.display = "none";
+  })();
 });
 
-function updateWarningMessage() {
-  const count = ($textarea.value.match(/!!!!!/g) || []).length;
+function readFileAsText(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject(new Error("파일이 없습니다."));
+    }
+
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = (ev: ProgressEvent<FileReader>) => {
+      resolve(ev.target?.result as string);
+    };
+
+    reader.onerror = () => {
+      reject(new Error("파일을 읽는 도중 에러가 발생했습니다."));
+    };
+  });
+}
+
+function getArticleElementFromText(text: string) {
+  const domparser = new DOMParser();
+  const doc = domparser.parseFromString(text, "text/html");
+  return doc.querySelector("article");
+}
+
+$textarea.addEventListener("keyup", () => {
+  updateWarningMessage($textarea.value);
+});
+
+function updateWarningMessage(text: string) {
+  const count = (text.match(/!!!!!/g) || []).length;
   $warning.innerHTML = `<strong>${count}개 항목의 수정이 필요합니다</strong> (ctrl + f로 !!!!! 를 검색하세요)`;
   $warning.style.display = count > 0 ? "block" : "none";
 }
